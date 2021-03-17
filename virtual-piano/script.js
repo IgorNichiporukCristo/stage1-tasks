942cd2635a2d09296909481b70e2672ad45bbda6
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2021-03-18 01:56:32 +0300

    feat:script

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,25 +1,25 @@
 const piano = document.querySelector(".piano")
 const pianokey = document.querySelectorAll(".piano-key")
 
-piano.addEventListener("mousedown", () => {
+const addEvent = () => {
     pianokey.forEach((elem) => {
         elem.addEventListener("mouseover", (event) => {
             playAudio(event)
         })
     })
+}
 
-})
-piano.addEventListener("mouseup", () => {
+const removeEvent = () => {
     pianokey.forEach((elem) => {
-        elem.removeEventListener("mouseout", (event) => {
-            return false;
-        })
+        elem.removeEventListener("mouseout", addEvent)
     })
-})
+}
 
-
-playAudio = (event) => {
+const playAudio = (event) => {
     let note = document.getElementById(event.target.dataset.note);
     note.currentTime = 0;
     note.play();
 }
+
+piano.addEventListener("mousedown", addEvent, false) 
+piano.addEventListener("mouseup", removeEvent)
