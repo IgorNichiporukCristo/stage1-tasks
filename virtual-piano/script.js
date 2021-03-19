55e133c03ac09d02bf4f8e8fdfe42faacf600480
igor <inichiporuk1992@gmail.com>
2021-03-19 17:22:58 +0300

    feat: added mouseevent

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,31 +1,46 @@
 const piano = document.querySelector(".piano")
 const pianokey = document.querySelectorAll(".piano-key")
 
-const playAudio = (event) => {
-    let note = document.getElementById(event.target.dataset.note);
-    console.log(note)
-    console.log(event.target)
-    console.log(document.querySelectorAll(".piano-key"))
+const playEventMouse = (event) => {
+    let note = document.getElementById(event.target.dataset.letter.toLowerCase());
     note.currentTime = 0;
     note.play();
+    event.target.classList.add("piano-key-active");
+    let timeout = () => {
+        event.target.classList.remove("piano-key-active");
+    }
+    setTimeout(timeout, 500);
 }
 
 const addEventMouse = (event) => {
-    playAudio(event)
+    playEventMouse(event);
     pianokey.forEach((elem) => {
-        elem.addEventListener("mouseover", playAudio)
+        elem.addEventListener("mouseover", playEventMouse);
     })
 }
 const removeEventMouse = () => {
     pianokey.forEach((elem) => {
-        elem.removeEventListener("mouseover", playAudio)
+        elem.removeEventListener("mouseover", playEventMouse);
     })
 }
 
-const addEventKeys = (event) => {
-
+const playEventKey = (event) => {
+    if (!event.repeat) {
+        let note = document.getElementById(event.code[3].toLowerCase());
+        note.currentTime = 0;
+        note.play();
+        pianokey.forEach((elem) => {
+            if (event.code[3] == elem.dataset.letter) {
+                elem.classList.add("piano-key-active");
+                let timeout = () => {
+                    elem.classList.remove("piano-key-active");
+                }
+                setTimeout(timeout, 500);
+            }
+        })
+    }
 }
 
-window.addEventListener('keydown', addEventKeys, false);
-piano.addEventListener("mousedown", addEventMouse, false)
-piano.addEventListener("mouseup", removeEventMouse, false)
+window.addEventListener('keydown', playEventKey, false);
+piano.addEventListener("mousedown", addEventMouse, false);
+piano.addEventListener("mouseup", removeEventMouse, false);
